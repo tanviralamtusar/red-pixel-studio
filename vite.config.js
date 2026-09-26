da4a314a2466845699d6c19431146e1a46478c9d
tanviralamtusar <77721756+tanviralamtusar@users.noreply.github.com>
2026-09-26 21:25:17 +0600

    Refactor HTML files to use module script type and update footer structure; add Vite configuration for multi-page setup

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        portfolio: 'portfolio.html',
+        services: 'services.html'
+      }
+    }
+  }
+};
